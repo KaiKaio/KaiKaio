155bd77675bd642036d7ff9a1aea17073a84919e
Chenkaiwei <1035759181@qq.com>
2020-11-26 13:11:10 +0800

    HTTPS配置

--- config/api.config.js
+++ config/api.config.js
@@ -11,7 +11,7 @@ const apiConfig = {
     baseUrl: 'http://localhost:4000/api',
   },
   production: {
-    baseUrl: 'http://www.kaikaio.com/api',
+    baseUrl: 'https://api.kaikaio.com/api',
   }
 }
 
