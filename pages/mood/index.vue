a0a383391d724af172f037a03fa400a390762576
Chenkaiwei <0027016226@dayu.work>
2021-06-25 09:31:46 +0800

    Feat: 新增短句Page

---
+++ pages/mood/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="mood">
+    Mood
+  </div>
+</template>
+
+
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: "About",
+  head() {
+    return {
+      title: `短句 | KaiKaio`,
+    }
+  },
+
+  data(){
+    return {
+    }
+  },
+
+  computed: {
+    ...mapState({
+
+    }),
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+    ...mapActions({
+      musicSwich: 'Music/musicSwich'
+    })
+  }
+};
+</script>
+
+<style lang="scss">
+#mood {
+}
+</style>
